aa4c2dddf844ba9873e649144b11a8d942979666
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-18 11:39:18 -0400

    added if statements for character builder function, also added function within the html button

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,34 +1,23 @@
 
-var generateBtn = document.querySelector("#generate");
+var password=document.querySelector("password");
 
-// Write password to the #password input
-/*function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-  if(password !== undefined) {
-    passwordText.value = password;
-  } else {
-    return
-  }
-}*/
-
-//unclear where to use var vs let - using lets for now
-function gatherPromptsforPassword() {
-  let passwordLength = prompt('How many characters should you password be? (must be between 8 and 128)');
+function gatherPromptsForPassword() {
+  let passwordLength = prompt('How many characters should your password be? (must be between 8 and 128)');
   
   if(passwordLength >= 8 && passwordLength <= 128) {
-    let passwordHasUppercase = confirm('Will the password contain uppercase letters? (Cancel for no uppercase)');
-    let passwordHasLowerCase = confirm('Will the password contain lowercase letters? (Cancel for no lowercase)');
-    let passwordHasNumeric = confirm('Will the password contain numeric values? (Cancel for no numeric)');
-    let passwordHasSpecialCharacters = confirm('Will the password contain special characters? (Cancel for no special)');
-    console.log(passwordHasUppercase , passwordHasLowerCase, passwordHasNumeric, passwordHasSpecialCharacters);
+    let includeUppercase = confirm('Will the password contain uppercase letters? (Cancel for no uppercase)');
+    let includeLowerCase = confirm('Will the password contain lowercase letters? (Cancel for no lowercase)');
+    let includeNumeric = confirm('Will the password contain numeric values? (Cancel for no numeric)');
+    let includeSpecialCharacters = confirm('Will the password contain special characters? (Cancel for no special)');
+    
+    console.log(includeUppercase , includeLowerCase, includeNumeric, includeSpecialCharacters);
 
     // // Generate the password based on the above variable information.
     //this will run the function above to generate password - which in turn should run the prompts function. 
-    if (passwordHasUpperCase) {
+    if (includeUppercase) {
     	generateAllCapsPassword();
     } else {
-    		generatePassword(6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    		generatePassword(12, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     }
     
   } else {
@@ -36,39 +25,61 @@ function gatherPromptsforPassword() {
   }
 }
 
-
 //trying new method here. pulling all characters into one string. will have to figure out how to divy them out based on user preference later.
-function generatePassword() {
+function generatePassword(userlength, charType) {
   let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let passwordLength = 6; //need to figure out how to make the length user picked instead
+  let passwordLength = 12;
   let password = "";
-  for(let i = 0; i <= passwordLength; i++){
-    let randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(random.Number, randomNumber +1);
+  
+ 	for (let i = 0; i <= passwordLength; i++) {
+   let randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
   }
-  document.querySelector("#password").value = password;
+  
+  document.getElementById("password").value = password;
 }
 
 
 function generateAllCapsPassword() {
-	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let chars = charTypeBuilder();
   let passwordLength = 6;
   let password = "";
+  
   
  	for (let i = 0; i <= passwordLength; i++) {
    let randomNumber = Math.floor(Math.random() * chars.length);
    password += chars.substring(randomNumber, randomNumber +1);
 
 
-//seeing if making a character selector works instead of above function?
+// still broken somewhere here
    function charTypeBuilder() {
-    passwordHasUpperCase
-    passwordHasLowerCase
-    passwordHasNumeric
-    passwordHasSpecialCharacters
-    if(passwordAllTypes) {
-      return "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    lowercase = "abcdefghijklmnopqrstuvwxyz";
+    numbers  = "0123456789";
+    special = "!@#$%^&*()";
+
+    if(includeUpperCase) {
+      charList = uppercase;
+      console.log(charList);
     }
+  
+    if(includeLowerCase) {
+      charList = lowercase;
+      console.log(charList);
+    }
+  
+    if(includeNumeric) {
+      charlist = numbers;
+      console.log(charList);
+    }
+  
+    if(includeSpecialCharacters) {
+      charlist = special;
+      
+      console.log(charList);
+    }
+  
+    return charList;
   }
 
 // Add event listener to generate button
