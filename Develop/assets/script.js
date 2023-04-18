61e7864b1d0cb6d2fb7ba5060c66d5fd81fe5797
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-18 18:00:23 -0400

    fixed syntax errors causing errors in functions, deleted generate password function - need to reassess

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,69 +1,46 @@
 
-var password = document.querySelector("password");
+
+var password = document.querySelector("#password");
+
 //when to use var vs let vs const
 function gatherPromptsForPassword() {
-  
+  console.log('on click works');
   let passwordLength = prompt('How many characters should your password be? (must be between 8 and 128)');
   
   if(passwordLength >= 8 && passwordLength <= 128) {
     let charList = "";
-
-    let includeUppercase = confirm('Will the password contain uppercase letters? (Cancel for no uppercase)');
-      
+    
+    let includeUpperCase = confirm('Will the password contain uppercase letters? (Cancel for no uppercase)');
+    let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       if(includeUpperCase) {
         charList += uppercase;
         console.log(charList);
       }
   
     let includeLowerCase = confirm('Will the password contain lowercase letters? (Cancel for no lowercase)');
+    let lowercase = "abcdefghijklmnopqrstuvwxyz";
       if(includeLowerCase) {
         charList += lowercase;
         console.log(charList);
       }
   
     let includeNumeric = confirm('Will the password contain numeric values? (Cancel for no numeric)');
+    let numbers  = "0123456789";
       if(includeNumeric) {
-        charlist = numbers;
+        charlist += numbers;
         console.log(charList);
       }
+      
     let includeSpecialCharacters = confirm('Will the password contain special characters? (Cancel for no special)');
+    let special = "!@#$%^&*()";
       if(includeSpecialCharacters) {
-        charlist = special;
+        charlist += special;
         console.log(charList);
       }
-  
-    return charList;
-  }
 
-    function generatePassword() {
-      let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      let passwordLength = 6;
-      let password = "";
-      
-       for (let i = 0; i <= passwordLength; i++) {
-       let randomNumber = Math.floor(Math.random() * chars.length);
-       password += chars.substring(randomNumber, randomNumber +1);
-      }
-      
-      document.getElementById("password").value = password;
-    }
+    // document.getElementById("password").value = password;
 
-    //this will run the function above to generate password - which in turn should run the prompts function. 
-    generatePassword();
-    
   } else {
     alert('You must enter a value between 8-128!');
   }
 }
-
-// is this function necessary? - where else can I add these variables?
-   function charTypeBuilder() {
-    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    lowercase = "abcdefghijklmnopqrstuvwxyz";
-    numbers  = "0123456789";
-    special = "!@#$%^&*()";
-    let charList = "";
-   }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
